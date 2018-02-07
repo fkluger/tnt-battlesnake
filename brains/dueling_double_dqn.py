097f074543a1e9c16901e42baa0543368b94c8c4
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-02-07 11:16:59 +0100

    feat(*): add dropout to dueling network

--- brains/dueling_double_dqn.py
+++ brains/dueling_double_dqn.py
@@ -1,5 +1,6 @@
+import math
 from keras import Model, Input
-from keras.layers import Conv2D, Flatten, Dense, Lambda, Add
+from keras.layers import Conv2D, Flatten, Dense, Lambda, Add, Dropout
 from keras.optimizers import RMSprop
 import tensorflow as tf
 
@@ -9,6 +10,19 @@ from .huber_loss import huber_loss
 
 class DuelingDoubleDQNBrain(DoubleDQNBrain):
 
+    dropout_layers = []
+    rate = 1.0
+    steps = 0
+
+    def predict(self, state, target=False):
+        self.steps += 1
+        self.rate = 0 + math.exp(-1e-6 * self.steps)
+        for layer in self.dropout_layers:
+            layer.rate = self.rate
+        if self.steps % 10000 == 0:
+            print(f'Dropout rate: {self.rate}')
+        return super().predict(state, target)
+
     def create_model(self):
         inputs = Input(shape=self.input_shape)
         net = Conv2D(32, 8, activation='relu', padding='same')(inputs)
@@ -16,8 +30,12 @@ class DuelingDoubleDQNBrain(DoubleDQNBrain):
         net = Conv2D(64, 3, activation='relu')(net)
         net = Flatten()(net)
         advt = Dense(512, activation='relu')(net)
+        advt = Dropout(self.rate)(advt)
+        self.dropout_layers.append(advt)
         advt = Dense(self.num_actions)(advt)
         value = Dense(512, activation='relu')(net)
+        value = Dropout(self.rate)(value)
+        self.dropout_layers.append(value)
         value = Dense(1)(value)
         # now to combine the two streams
         advt = Lambda(lambda advt: advt - tf.reduce_mean(advt, axis=-1, keepdims=True))(advt)

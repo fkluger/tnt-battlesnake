7d661fb0805ab047655f1505c7ff8d86253b2f3f
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-06-12 08:23:44 +0200

    fix(*): add logging directory

--- main_utils.py
+++ main_utils.py
@@ -1,10 +1,14 @@
 import logging
 import time
+import os
 
+LOGGING_DIR = './logs'
 
 def wrap_main(main_fn):
     try:
         main_fn()
     except Exception as e:
-        logging.root.addHandler(logging.FileHandler(filename=f'error-{time.time()}.log'))
+        if not os.path.exists(LOGGING_DIR):
+            os.makedirs(LOGGING_DIR)
+        logging.root.addHandler(logging.FileHandler(filename=f'{LOGGING_DIR}/error-{time.time()}.log'))
         logging.exception(e)

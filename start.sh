dd340dd99cca1cd20ae1346fea1dc51eb41ff5d5
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-06-12 08:13:52 +0200

    fix(main): just log the exception

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 screen -dmS "learner" "python" "main_learner.py"
+echo "Started learner thread."
 sleep 3
 for i in `seq 1 $1`;
 do
     screen -dmS "actor-$i" "python" "main_actor.py"
+    echo "Started actor $i"
     sleep 1
 done 

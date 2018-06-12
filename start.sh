4b685fd476ae9d0cc5e984cfeb89518f84b376d3
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-06-12 10:13:33 +0200

    fix(start): use correct actor index param

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Started learner thread."
 sleep 3
 for i in `seq 1 $1`;
 do
-    screen -dmS "actor-$i" "python" "main_actor.py"
+    screen -dmS "actor-$i" "python" "main_actor.py --actor_index $i"
     echo "Started actor $i"
     sleep 1
 done 

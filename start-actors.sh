1d0fac6760baecc713f719eb15cf753d9d111d3f
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-06-22 19:31:56 +0200

    fix(*): split up actor and learner start scripts

--- start-actors.sh
+++ start-actors.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-screen -dmS "learner" "python" "main_learner.py"
-echo "Started learner thread."
-sleep 3
 for i in `seq 1 $1`;
 do
     screen -dmS "actor-$i" "python" "main_actor.py" "--actor_index" "$i"
     echo "Started actor $i"
     sleep 1
-done 
+done
---
+++ start-learner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+screen -dmS "learner" "python" "main_learner.py"
+echo "Started learner thread."

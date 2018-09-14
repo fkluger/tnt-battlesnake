fix(dnd): use broadcasting instead of tiling

environment/serializers/state_serializer.py
@@ -27,7 +27,7 @@ class StateSerializer:
     def on_reset(self):
         self.last_frames_per_snake = [
             deque(
-                np.zeros([self.stacked_frames, self.width, self.height], dtype=np.float32),
+                np.zeros([self.stacked_frames, self.width, self.height], dtype=np.int8),
                 self.stacked_frames,
             )
             for snake_idx in range(self.snakes)
@@ -36,7 +36,7 @@ class StateSerializer:
     def serialize(self, snake_perspective, state):
         if snake_perspective == 0:
             for snake_idx in range(self.snakes):
-                current_state = np.zeros([state.width, state.height], dtype=np.float32)
+                current_state = np.zeros([state.width, state.height], dtype=np.int8)
                 for x in range(state.width):
                     for y in range(state.height):
                         if (

nec/build_graph.py
@@ -12,51 +12,52 @@ def build_graph(
     num_actions: int,
     key_length: int,
 ):
-    observations_ph = tf.placeholder(
-        name="observations", shape=[None] + input_shape, dtype=tf.float32
-    )
-    q_values_ph = tf.placeholder(name="q_values", shape=[None], dtype=tf.float32)
-    actions_ph = tf.placeholder(name="actions", shape=[None], dtype=tf.int32)
-    target_q_values_ph = tf.placeholder(
-        name="target_q_values", shape=[None], dtype=tf.float32
-    )
-
-    keys = encoder(observations_ph)
-
-    q_values = [dnd.lookup(keys) for dnd in dnds]
-    best_actions = tf.argmax(tf.transpose(q_values), axis=1, name="best_actions")
-
-    q_values_selected = tf.gather(q_values, actions_ph, name="q_values_selected")
-    loss = tf.reduce_sum(tf.square(target_q_values_ph - q_values_selected))
-    train_op = optimizer.minimize(loss)
-
-    def train(observations, actions, target_q_values):
-        error, _ = tf.get_default_session().run(
-            [loss, train_op],
-            {
-                observations_ph: observations,
-                actions_ph: actions,
-                target_q_values_ph: target_q_values,
-            },
+    with tf.variable_scope("nec", reuse=None):
+        observations_ph = tf.placeholder(
+            name="observations", shape=[None] + input_shape, dtype=tf.int8
         )
-        return error
-
-    def act(observations):
-        return tf.get_default_session().run(
-            [best_actions, q_values], {observations_ph: observations}
+        q_values_ph = tf.placeholder(name="q_values", shape=[None], dtype=tf.float32)
+        actions_ph = tf.placeholder(name="actions", shape=[None], dtype=tf.int32)
+        target_q_values_ph = tf.placeholder(
+            name="target_q_values", shape=[None], dtype=tf.float32
         )
 
-    def write(observations_per_action, q_values_per_action):
-        return [
-            tf.get_default_session().run(
-                dnds[a].write(keys, q_values_ph),
+        keys = encoder(tf.to_float(observations_ph))
+
+        q_values = [dnd.lookup(keys) for dnd in dnds]
+        best_actions = tf.argmax(tf.transpose(q_values), axis=1, name="best_actions")
+
+        q_values_selected = tf.gather(q_values, actions_ph, name="q_values_selected")
+        loss = tf.reduce_sum(tf.square(target_q_values_ph - q_values_selected))
+        train_op = optimizer.minimize(loss)
+
+        def train(observations, actions, target_q_values):
+            error, _ = tf.get_default_session().run(
+                [loss, train_op],
                 {
-                    observations_ph: observations_per_action[a],
-                    q_values_ph: q_values_per_action[a],
+                    observations_ph: observations,
+                    actions_ph: actions,
+                    target_q_values_ph: target_q_values,
                 },
             )
-            for a in range(num_actions)
-            if len(observations_per_action[a])
-        ]
+            return error
 
-    return train, act, write
+        def act(observations):
+            return tf.get_default_session().run(
+                [best_actions, q_values], {observations_ph: observations}
+            )
+
+        def write(observations_per_action, q_values_per_action):
+            return [
+                tf.get_default_session().run(
+                    dnds[a].write(keys, q_values_ph),
+                    {
+                        observations_ph: observations_per_action[a],
+                        q_values_ph: q_values_per_action[a],
+                    },
+                )
+                for a in range(num_actions)
+                if len(observations_per_action[a])
+            ]
+
+        return train, act, write

nec/dnd.py
@@ -160,9 +160,7 @@ class DifferentiableNeuralDictionary:
         distances = tf.square(
             tf.norm(
                 tf.expand_dims(self.keys, 0)  # [1, capacity, key_length]
-                - tf.tile(
-                    tf.expand_dims(keys, 1), [1, self.capacity, 1]
-                ),  # [batch_size, capacity, key_length]
+                - tf.expand_dims(keys, 1),  # [batch_size, 1, key_length]
                 axis=-1,
             )
         )

nec/dnd_test.py
@@ -8,8 +8,8 @@ class DifferentiableNeuralDictionaryTest(tf.test.TestCase):
     def testWrite(self):
         batch_size = 2
         key_length = 1
-        capacity = 20
-        num_nearest_neighbours = 8
+        capacity = 500000
+        num_nearest_neighbours = 50
         dnd = DifferentiableNeuralDictionary(
             0, capacity, key_length, num_nearest_neighbours, 1e-3, 0.1
         )
@@ -30,17 +30,7 @@ class DifferentiableNeuralDictionaryTest(tf.test.TestCase):
             for i in range(10):
                 sess.run(write_op, {keys: np.array([[i], [i]]), values: np.array([i, i])})
 
-            print("Keys\n", dnd.keys.eval())
-            print("Values\n", dnd.values.eval())
-            print("Ages\n", dnd.ages.eval())
-
             sess.run(lookup_op, {keys: np.array([[1], [1]])})
-
-            print("Keys\n", dnd.keys.eval())
-            print("Values\n", dnd.values.eval())
-            print("Ages\n", dnd.ages.eval())
-
-            sess.run(lookup_op, {keys: np.array([[5], [5]])})
 
             print("Keys\n", dnd.keys.eval())
             print("Values\n", dnd.values.eval())

tensorboard_logger.py
@@ -19,13 +19,13 @@ class Metric:
 
 
 class TensorboardLogger:
-    def __init__(self, output_directory, actor_idx=None):
+    def __init__(self, output_directory, actor_idx=None, graph=None):
         if actor_idx is not None:
             self.output_directory = f"{output_directory}/actor-{actor_idx}"
         else:
             self.output_directory = f"{output_directory}/learner"
 
-        self.writer = tf.summary.FileWriter(self.output_directory)
+        self.writer = tf.summary.FileWriter(self.output_directory, graph=graph)
         if actor_idx == 1:
             self._init_custom_scalar_layout()
 
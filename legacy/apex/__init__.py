4ebe60a53d8a632623b719cfbcfcb61f69b85ace
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-12-11 11:23:12 +0100

    chore(*): remove legacy directory and add battlesnake adapter directory

--- legacy/apex/__init__.py
+++
@@ -1,5 +0,0 @@
-from apex.actor import AbstractActor
-from apex.learner import AbstractLearner
-from apex.configuration import Configuration
-from apex.models import Experience, Observation
-from apex.enemy_actor import EnemyActor
